5ea43bf02b7294d1fa3ef777b072c9e6efc24b7d
Eduardo Aleixo <eduardo@pyroscope.io>
2023-12-11 21:09:01 +0000

    remove from db files that don't have audio

--- hacks/example-sentence/script.ts
+++ hacks/example-sentence/script.ts
@@ -2,9 +2,11 @@
 // Let's populate the database
 // Using the `all_v11.json` file we got from https://sentencesearch.neocities.org/data/all_v11.json
 
+import { existsSync } from "fs";
 import { Database } from "sqlite";
 import * as datastore from "../db";
 import data from "./all_v11.json";
+import * as path from "path";
 
 async function run() {
   // open the database
@@ -13,15 +15,22 @@ async function run() {
 
   // TODO: skip those that don't have all items
   for (let i = 0; i < data.length; i++) {
-    datastore.insertExampleSentence(db, {
-      source: data[i].source,
-      audioJapanese: data[i].audio_jap,
-      japaneseSentence: data[i].jap,
-      englishSentence: data[i].eng,
-    });
+    if (audioFileExists(data[i].audio_jap)) {
+      datastore.insertExampleSentence(db, {
+        source: data[i].source,
+        audioJapanese: data[i].audio_jap,
+        japaneseSentence: data[i].jap,
+        englishSentence: data[i].eng,
+      });
+    }
   }
 }
 
+// My understanding is that all files from tango/n3 don't exist :\
+function audioFileExists(src) {
+  return existsSync(path.join(__dirname, "../../public/audio-files", src));
+}
+//
 if (require.main == module) {
   run();
 }
