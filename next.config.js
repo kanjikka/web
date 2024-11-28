e5121b583fab6d2020dd3908bd737233c0635515
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2024-11-28 15:22:54 +0000

    deploy under a base path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  basePath:
+    process.env.NODE_ENV === "development"
+      ? undefined
+      : "/japanese-writing-practice",
   output: "export",
   typescript: {
     // TODO: fix this
