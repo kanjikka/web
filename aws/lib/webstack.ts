f8122dbca029333184dc3691306dae7b704a4ae7
eduardo aleixo <eh-am@users.noreply.github.com>
2025-09-21 19:20:58 +0100

    use certificate (#49)

--- aws/lib/webstack.ts
+++ aws/lib/webstack.ts
@@ -1,12 +1,45 @@
 import { App, CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Nextjs } from "cdk-nextjs-standalone";
 
+type Props = StackProps & {
+  domainName: string;
+  alternativeDomainNames: string[];
+};
 export class WebStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
+
+    const zone = cdk.aws_route53.HostedZone.fromLookup(
+      this,
+      "ZoneCertificate",
+      {
+        domainName: props.domainName,
+      }
+    );
+
+    const certificate = new cdk.aws_certificatemanager.DnsValidatedCertificate(
+      this,
+      "SiteCertificate",
+      {
+        domainName: props.domainName,
+        subjectAlternativeNames: props.alternativeDomainNames,
+        hostedZone: zone,
+        // It needs to be us-east-1 so that cloudfront works
+        region: "us-east-1",
+        validation:
+          cdk.aws_certificatemanager.CertificateValidation.fromDns(zone),
+      }
+    );
+
     const nextjs = new Nextjs(this, "Nextjs", {
       nextjsPath: "../", // relative path from your project root to NextJS
+      domainProps: {
+        domainName: props.domainName,
+        certificate: certificate,
+        hostedZone: zone,
+      },
     });
     new CfnOutput(this, "CloudFrontDistributionDomain", {
       value: nextjs.distribution.distributionDomain,
