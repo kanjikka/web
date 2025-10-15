60f7ccf131e92189f010cae8b67851e44d61cabc
Eduardo Aleixo <eduardoaleixomartins@gmail.com>
2025-10-15 16:11:29 +0100

    add how it works section

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/app/components/Home/Hero/Hero";
+import { HowItWorks } from "@/app/components/HowItWorks/HowItWorks";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Hero />
+        <HowItWorks />
       </main>
     </div>
   );
